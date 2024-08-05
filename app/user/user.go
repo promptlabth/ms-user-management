999e0da81be8950d19b1264f0b78b54f313b5148
Pet002 <pet.14336@gmail.com>
2024-08-05 23:00:39 +0700

    update tag gorm

--- app/user/user.go
+++ app/user/user.go
@@ -1,8 +1,8 @@
 package user
 
 type UserEntity struct {
-	ID          int64   `gorm:"column:id,primaryKey"`
-	FirebaseId  string  `gorm:"column:firebase_id,uniqueIndex"`
+	ID          int64   `gorm:"column:id;primaryKey"`
+	FirebaseId  string  `gorm:"column:firebase_id;uniqueIndex"`
 	Name        string  `gorm:"column:name"`
 	Email       *string `gorm:"column:email"`
 	ProfilePic  *string `gorm:"column:profilepic"`
@@ -17,7 +17,7 @@ func (UserEntity) TableName() string {
 }
 
 type PlanEntity struct {
-	ID          int64   `gorm:"column:id,primaryKey"`
+	ID          int64   `gorm:"column:id;primaryKey"`
 	PlanType    string  `gorm:"column:plan_type"`
 	MaxMessages int64   `gorm:"column:max_messages"`
 	ProductId   *string `gorm:"column:product_id"`
@@ -28,7 +28,7 @@ func (PlanEntity) TableName() string {
 }
 
 type UserBalanceMessage struct {
-	FirebaseId     string `gorm:"column:firebase_id,primaryKey"`
+	FirebaseId     string `gorm:"column:firebase_id;primaryKey"`
 	BalanceMessage int64  `gorm:"column:balance_message"`
 }
 
